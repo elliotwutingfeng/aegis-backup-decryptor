465d7a27469f3af44bc077b2bb579be03380c69f
Wu Tingfeng <wutingfeng@outlook.com>
2024-01-15 13:16:42 +0800

    initial commit

---
+++ lib/decrypt.rb
@@ -0,0 +1,208 @@
+# CLI tool to decrypt backup files exported from the Aegis Authenticator app
+# Copyright (C) 2024 Wu Tingfeng <wutingfeng@outlook.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+require 'base64'
+require 'io/console'
+require 'json'
+require 'openssl'
+
+ENCRYPTION_CIPHER = 'aes-256-gcm'.freeze
+
+# DrillPatch provides method `drill` similar to Hash.dig
+# but returns nil instead of raising TypeError if it encounters
+# an intermediate step that is not a Hash.
+module DrillPatch
+  def drill(*args)
+    node = self
+    args.each do |arg|
+      return nil unless (node.is_a? Hash) && (node.key? arg)
+
+      node = node[arg]
+    end
+    node
+  end
+end
+Hash.include DrillPatch
+
+def terminate(message)
+  warn message
+  exit 1
+end
+
+def assert_is_hash(obj)
+  return if obj.is_a? Hash
+
+  terminate 'Invalid vault file. Top-level is not Hash.'
+end
+
+#
+# Parse `plain_text` string as JSON object.
+#
+# @param [String] plain_text Encoded JSON string
+#
+# @return [BasicObject] JSON object
+#
+def parse_json(plain_text)
+  JSON.parse(plain_text, :symbolize_names => true)
+rescue JSON::ParserError => e
+  terminate e.message
+end
+
+#
+# Extract valid password slots from JSON Hash `obj`.
+# Valid password slots match the Aegis Authenticator password slot schema.
+#
+# @param [String] obj JSON Hash
+#
+# @return [Hash] Parameters needed to decrypt the cipher text
+#
+def extract_password_slots(obj)
+  assert_is_hash(obj)
+  slots = obj.drill(:header, :slots)
+  terminate 'Invalid vault file. No valid password slots found.' unless slots.is_a? Array
+
+  password_slots = slots.select do |slot|
+    return false if slot.drill(:type) != 1
+    return false unless slot.drill(:key).is_a? String
+    return false unless slot.drill(:key_params, :nonce).is_a? String
+    return false unless slot.drill(:key_params, :tag).is_a? String
+    return false unless slot.drill(:n).is_a? Integer
+    return false unless slot.drill(:r).is_a? Integer
+    return false unless slot.drill(:p).is_a? Integer
+    return false unless slot.drill(:salt).is_a? String
+
+    true
+  end
+
+  terminate 'Invalid vault file. No valid password slots found.' if password_slots.empty?
+
+  password_slots
+end
+
+#
+# Extract cipher text from `obj` JSON Hash.
+#
+# @param [String] obj JSON Hash
+#
+# @return [Hash] Cipher text as bytes
+#
+def get_db(obj)
+  assert_is_hash(obj)
+  db = obj[:db]
+  terminate 'Invalid vault file. No db found.' unless db.is_a? String
+  Base64.strict_decode64 db
+end
+
+#
+# Extract AES-GCM initialization vector and AES-GCM authentication tag from `obj` JSON Hash.
+#
+# @param [String] obj JSON Hash
+#
+# @return [Hash] AES-GCM initialization vector and authentication tag
+#
+def get_vault_params(obj)
+  assert_is_hash(obj)
+
+  iv = obj.drill(:header, :params, :nonce)
+  terminate 'Invalid vault file. No initialization vector found.' unless iv.is_a? String
+
+  auth_tag = obj.drill(:header, :params, :tag)
+  terminate 'Invalid vault file. No authentication tag found.' unless auth_tag.is_a? String
+
+  { :iv => [iv].pack('H*'), :auth_tag => [auth_tag].pack('H*') }
+end
+
+#
+# Derive the vault master key by trying out `password`
+# on all `password_slots` until one succeeds.
+#
+# @param [String] password Backup file password in plaintext
+# @param [Array] password_slots Array of Hash where each Hash may contain parameters needed to derive the master key
+#
+# @return [String] Derived master key as bytes
+#
+def derive_master_key(password, password_slots)
+  password_slots.each do |slot|
+    decipher = OpenSSL::Cipher.new ENCRYPTION_CIPHER
+    decipher.decrypt
+    decipher.key = OpenSSL::KDF.scrypt(password, :salt => [slot[:salt]].pack('H*'), :N => slot[:n],
+                                                 :r => slot[:r], :p => slot[:p], :length => 32)
+    decipher.iv = [slot.drill(:key_params, :nonce)].pack 'H*'
+    decipher.auth_tag = [slot.drill(:key_params, :tag)].pack 'H*'
+    decipher.padding = 0
+
+    begin
+      return decipher.update([slot[:key]].pack('H*')) + decipher.final
+    rescue OpenSSL::Cipher::CipherError
+      nil
+    end
+  end
+  terminate 'Failed to derive master key. Wrong password?'
+end
+
+#
+# Decrypt `cipher_text` and return the plaintext result as String
+#
+# @param [String] cipher_text Encrypted text as bytes to be decrypted
+# @param [String] master_key `cipher_text`'s master key as bytes
+# @param [String] iv AES-GCM initialization vector as bytes
+# @param [String] auth_tag AES-GCM authentication tag as bytes
+#
+# @return [String] Decrypted `cipher_text`
+#
+def decrypt_ciphertext(cipher_text, master_key, iv, auth_tag)
+  decipher = OpenSSL::Cipher.new ENCRYPTION_CIPHER
+  decipher.decrypt
+  decipher.key = master_key
+  decipher.iv = iv
+  decipher.auth_tag = auth_tag
+  decipher.padding = 0
+
+  decipher.update(cipher_text) + decipher.final
+end
+
+#
+# Prompt terminal user for password.
+# A drop-in replacement for $stdin.getpass for older Ruby versions.
+#
+# @param [String] prompt Message prompt to display
+#
+# @return [String] Password
+#
+def getpass(prompt)
+  $stderr.write prompt # Display prompt without adding prompt to stdout.
+  password = $stdin.noecho(&:gets).chomp
+  $stderr.puts # Display newline without adding newline to stdout.
+  password
+end
+
+def main
+  terminate 'Usage: decrypt.rb <filename>' if ARGV.length != 1
+
+  obj = parse_json File.read(ARGV[0], :encoding => 'utf-8')
+  password_slots = extract_password_slots(obj)
+  cipher_text = get_db(obj)
+  iv, auth_tag = get_vault_params(obj).values_at(:iv, :auth_tag)
+
+  password = getpass('Enter Aegis encrypted backup password: ')
+  master_key = derive_master_key(password, password_slots)
+  plain_text = decrypt_ciphertext(cipher_text, master_key, iv, auth_tag)
+  parse_json(plain_text) # Ensure plain_text is valid JSON.
+
+  $stdout.write plain_text
+end
+
+main if __FILE__ == $PROGRAM_NAME
---
+++ spec/decrypt_spec.rb
@@ -0,0 +1,135 @@
+# CLI tool to decrypt backup files exported from the Aegis Authenticator app
+# Copyright (C) 2024 Wu Tingfeng <wutingfeng@outlook.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+require 'spec_helper'
+require 'decrypt'
+
+# See https://michaelay.github.io/blog/2014/12/15/suppress-stdout-and-stderr-when-running-rspec
+def silence
+  @original_stderr = $stderr
+  @original_stdout = $stdout
+
+  $stderr = $stdout = StringIO.new
+
+  yield
+
+  $stderr = @original_stderr
+  $stdout = @original_stdout
+  @original_stderr = nil
+  @original_stdout = nil
+end
+
+describe 'parse_json' do
+  it 'Parses valid JSON' do
+    expect(parse_json('{"a": [1, 2]}')).to eq({ :a => [1, 2] })
+  end
+  it 'Exit 1 if invalid JSON' do
+    silence do
+      expect { parse_json('') }.to raise_error(SystemExit) do |error|
+        expect(error.status).to eq(1)
+      end
+    end
+  end
+end
+
+describe 'get_vault_params' do
+  it 'Extracts vault parameters if valid' do
+    params = get_vault_params(JSON.parse('{
+      "header": {"params": {"nonce":"1", "tag": "2"}}
+    }', :symbolize_names => true))
+    expected = { :iv => ['1'].pack('H*'), :auth_tag => ['2'].pack('H*') }
+    params.each do |k, v|
+      expect(v).to eq(expected[k])
+    end
+  end
+  it 'Exit 1 if vault parameters are invalid' do
+    test_vectors = ['{}',
+                    '{"header":{"params":{"nonce":1,"tag":"2"}}}',
+                    '{"header":{"params":{"nonce":"1","tag":2}}}'].map do |s|
+      JSON.parse(s, :symbolize_names => true)
+    end
+    silence do
+      test_vectors.each do |content|
+        expect { get_vault_params(content) }.to raise_error(SystemExit) do |error|
+          expect(error.status).to eq(1)
+        end
+      end
+    end
+  end
+end
+
+describe 'extract_password_slots' do
+  it 'Exit 1 if no valid slots are found' do
+    test_vectors = ['{}', '{"header":""}', '{"header":{"slots":[]}}'].map do |s|
+      JSON.parse(s, :symbolize_names => true)
+    end
+    silence do
+      test_vectors.each do |content|
+        expect { extract_password_slots(content) }.to raise_error(SystemExit) do |error|
+          expect(error.status).to eq(1)
+        end
+      end
+    end
+  end
+end
+
+describe 'get_db' do
+  it 'Exit 1 if no db string found' do
+    test_vectors = ['[]', '{}', '{"a": ""}'].map do |s|
+      JSON.parse(s, :symbolize_names => true)
+    end
+    silence do
+      test_vectors.each do |content|
+        expect { get_db(content) }.to raise_error(SystemExit) do |error|
+          expect(error.status).to eq(1)
+        end
+      end
+    end
+  end
+end
+
+describe 'main' do
+  it 'Correct password -> Decryption success' do
+    ARGV.replace ['test/encrypted_test.json']
+    allow($stdin).to receive(:noecho) { 'test' } # Backup file password
+    output = nil
+    expect($stderr).to receive(:puts)
+    expect($stdout).to receive(:write) { |arg| output = arg }
+    main
+    obj = JSON.parse(output, :symbolize_names => true)
+    expect(obj[:entries].length).to eq 7
+  end
+  it 'Wrong password -> Decryption failure' do
+    ARGV.replace ['test/encrypted_test.json']
+    allow($stdin).to receive(:noecho) { '' }
+    silence do
+      expect { main }.to raise_error(SystemExit) do |error|
+        expect(error.status).to eq(1)
+      end
+    end
+  end
+  it 'Accepts exactly 1 argument' do
+    test_vectors = [[], ['test/encrypted_test.json', 'another'], ['test/encrypted_test.json', 'yet another']]
+    silence do
+      test_vectors.each do |args|
+        ARGV.replace args
+        expect { main }.to raise_error(SystemExit) do |error|
+          expect(error.status).to eq(1)
+        end
+      end
+    end
+  end
+end
