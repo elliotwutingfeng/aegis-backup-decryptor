5eecdac15b4bba318940b458f0f558de12ba6328
Wu Tingfeng <wutingfeng@outlook.com>
2024-03-05 21:05:14 +0800

    Add tests for invalid vault format.

---
+++ lib/create_test_vault.rb
@@ -0,0 +1,84 @@
+# CLI tool to decrypt backup files exported from the Aegis Authenticator app
+# Copyright (C) 2024 Wu Tingfeng <wutingfeng@outlook.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+require 'base64'
+
+require_relative 'crypto'
+
+#
+# Encrypt vault with given AES-GCM parameters.
+# If successful, return encrypted vault as JSON String.
+#
+# The vault_key and vault_nonce are used to encrypt the plain_text, following which the vault_key itself is encrypted
+# with a meta_key derived from the password and salt, and a password_nonce. This encrypted_vault_key is then stored in
+# the first slot.
+#
+# Reference: https://github.com/beemdevelopment/Aegis/blob/master/docs/vault.md
+#
+# @param [String] plain_text Vault contents
+# @param [String] password First slot password
+# @param [String] salt First slot salt as bytes
+# @param [String] vault_key Vault master key as base64 strict encoded String
+# @param [String] password_nonce First slot AES-GCM initialization vector as bytes
+# @param [String] vault_nonce Vault AES-GCM initialization vector as bytes
+# @param [String] uuid First slot UUIDv4 identifier
+#
+# @return [String] Encrypted vault as JSON String
+#
+def encrypt_vault(plain_text, password, salt, vault_key, password_nonce, vault_nonce, uuid)
+  n = 32_768
+  r = 8
+  p = 1
+  length = 32
+
+  # Encrypt the plain_text
+  encrypted_plain_text, vault_tag = aes_gcm(plain_text, Base64.strict_decode64(vault_key), vault_nonce, true)
+
+  # Derive the meta_key
+  meta_key = derive_key(password, salt, n, r, p, length)
+
+  # Encrypt the vault_key using the meta_key
+  encrypted_vault_key, password_tag = aes_gcm(Base64.strict_decode64(vault_key), meta_key, password_nonce, true)
+
+  {
+    :version => 1,
+    :header => {
+      :slots => [
+        {
+          :type => 1,
+          :uuid => uuid,
+          :key => encrypted_vault_key.unpack1('H*'),
+          :key_params => {
+            :nonce => password_nonce.unpack1('H*'),
+            :tag => password_tag.unpack1('H*')
+          },
+          :n => n,
+          :r => r,
+          :p => p,
+          :salt => salt.unpack1('H*'),
+          :repaired => true
+        }
+      ],
+      :params => {
+        :nonce => vault_nonce.unpack1('H*'),
+        :tag => vault_tag.unpack1('H*')
+      }
+    },
+    :db => Base64.strict_encode64(encrypted_plain_text)
+  }.to_json
+rescue ArgumentError => e
+  terminate "Failed to encrypt vault. #{e.instance_of?(ArgumentError) ? e.message : 'Invalid parameters?'}"
+end
---
+++ spec/create_test_vault_spec.rb
@@ -0,0 +1,49 @@
+# CLI tool to decrypt backup files exported from the Aegis Authenticator app
+# Copyright (C) 2024 Wu Tingfeng <wutingfeng@outlook.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+require 'create_test_vault'
+require 'crypto'
+require 'json'
+require 'spec_helper'
+
+describe 'encrypt_vault' do
+  it 'Encrypts vault correctly' do
+    expected_plain_text = File.read('test/plaintext_test.json', :encoding => 'utf-8')
+    password = 'test'
+    salt = '27ea9ae53fa2f08a8dcd201615a8229422647b3058f9f36b08f9457e62888be1'
+    vault_key = 'W/Pupld1SxdtB26gcHjiKo3z4spavIhuLiX3zvJNEaY='
+    password_nonce = 'e9705513ba4951fa7a0608d2'
+    vault_nonce = '095fd13dee336fa56b4634ff'
+    uuid = 'a8325752-c1be-458a-9b3e-5e0a8154d9ec'
+
+    encrypted_vault_json = encrypt_vault(expected_plain_text, password, [salt].pack('H*'),
+                                         vault_key, [password_nonce].pack('H*'),
+                                         [vault_nonce].pack('H*'), uuid)
+    encrypted_vault = JSON.parse(encrypted_vault_json, :symbolize_names => true)
+    # Now decrypt it and check that its plaintext form matches the expected plaintext.
+    plain_text, = decrypt_ciphertext(get_db(encrypted_vault), password, encrypted_vault[:header][:slots],
+                                     [encrypted_vault[:header][:params][:nonce]].pack('H*'),
+                                     [encrypted_vault[:header][:params][:tag]].pack('H*'))
+    expect(plain_text).to eq expected_plain_text
+  end
+  it 'Fails to encrypt empty vault' do
+    silence('stderr') do
+      expect { encrypt_vault('', '', '', '', '', '', '') }.to raise_error(SystemExit) do |error|
+        expect(error.status).to eq(1)
+      end
+    end
+  end
+end
